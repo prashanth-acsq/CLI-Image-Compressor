82b65143d62c78be8104ab2ec3312c5b82a9706c
prashanth-acsq <prashanth@acsq.com.qa>
2022-05-18 08:30:06 +0300

    1. Fixed Paths; 2. Fixed quality value test; 3. Removed redundant args

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import sys
 from PIL import Image
 
 
-READ_PATH = "Files"
-SAVE_PATH = "Processed"
+READ_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Files")
+SAVE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Processed")
 
 
 def main():
@@ -13,12 +13,10 @@ def main():
     args_1: tuple = ("--file", "-f")
     args_2: tuple = ("--quality", "-q")
     args_3: tuple = ("--display", "-d")
-    args_4: tuple = ("--save", "-s")
 
     filename: str = None
     quality: float = None
     display: bool = False
-    save: bool = False
 
     if args_1[0] in sys.argv: filename = sys.argv[sys.argv.index(args_1[0]) + 1]
     if args_1[1] in sys.argv: filename = sys.argv[sys.argv.index(args_1[1]) + 1]
@@ -28,24 +26,20 @@ def main():
 
     if args_3[0] in sys.argv or args_3[1] in sys.argv: display = True
 
-    if args_4[0] in sys.argv or args_4[1] in sys.argv: save = True
-
     assert filename is not None, "No file specified"
     assert filename in os.listdir(READ_PATH), "File not found"
     assert quality is not None, "No quality specified"
-    assert quality < 0 and quality >= 100, "Quality out of range (0-100]"
+    assert quality > 0 and quality <= 100, "Quality out of range (0-100]"
 
     image: Image = Image.open(os.path.join(READ_PATH, filename))
 
-    if display: image.show()
-    if save: image.save(os.path.join(SAVE_PATH, filename.split(".")[0] + " - Compressed.jpg"), 
-                        optimizer=True, 
-                        quality=quality)
+    path = os.path.join(SAVE_PATH, filename.split(".")[0] + " - Compressed.jpg")
+    image.save(path, optimizer=True, quality=quality)
+
+    if display:
+        image: Image = Image.open(path)
+        image.show()
 
 
 if __name__ == "__main__":
     sys.exit(main() or 0)
-
-
-
-
